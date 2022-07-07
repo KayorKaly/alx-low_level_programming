c4eece370b8a0690ff6c85fa44706b0bdec103f8
KayorKaly <nnadoziekelechi@gmail.com>
2022-07-07 04:19:24 -0700

    debugging is time

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,9 +20,9 @@ int largest_number(int a, int b, int c)
     {
         largest = b;
     }
-    else
+    else if (c > b && b > c)
     {
-        largest = c;
+      largest = c;
     }
 
     return (largest);

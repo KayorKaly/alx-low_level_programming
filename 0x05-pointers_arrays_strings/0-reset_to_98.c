67d8e97d9ed84503341ae3be186d1ede35e160ba
KayorKaly <nnadoziekelechi@gmail.com>
2022-07-12 09:45:08 -0700

    This commit is for -reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updates pointer value
+ * @n: pointer variable
+ * Return: nothing;
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This readme is for 0x05-pointers_arrays_strings

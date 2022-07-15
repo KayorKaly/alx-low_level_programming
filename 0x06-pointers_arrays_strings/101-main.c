c59b80f892c9243875e67677a7714d7ab0fb0c47
KayorKaly <nnadoziekelechi@gmail.com>
2022-07-14 18:58:53 -0700

    101-main.c

--- 0x06-pointers_arrays_strings/101-main.c
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - check the code
  *
@@ -6,15 +7,15 @@
  */
 int main(void)
 {
-print_number(98);
-_putchar('\n');
-print_number(402);
-_putchar('\n');
-print_number(1024);
-_putchar('\n');
-print_number(0);
-_putchar('\n');
-print_number(-98);
-_putchar('\n');
-return (0);
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
+    return (0);
 }
